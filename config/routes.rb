ec68964e18536f196ce7398076593a336ff7c08b
ryommabjj113 <114906235+ryommabjj113@users.noreply.github.com>
2022-11-02 06:51:30 +0000

    Initial Commit1

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  
+  get 'top' => 'homes#top'
+  #get '/' => 'homes#top'
+  resources :books
+  root to: 'homes#top'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
